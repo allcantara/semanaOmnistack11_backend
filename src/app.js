8d4b1f17046d384f878df90b1029ee611861f321
allcantara <alcantara.desenvolvedor@gmail.com>
2020-04-28 00:07:01 -0300

    Remocao da restricao de url

--- src/app.js
+++ src/app.js
@@ -5,11 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://allcantara-bethehero.herokuapp.com/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use(errors());
